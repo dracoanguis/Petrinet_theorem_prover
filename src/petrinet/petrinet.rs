52e982eecfb416bbf6371115aaffcb2872b424c8
Ethan Arm <=>
2023-05-29 22:00:00 +0200

    Converted petrinet test to new format using link

--- src/petrinet/petrinet.rs
+++ src/petrinet/petrinet.rs
@@ -182,18 +182,18 @@ mod test {
             let places = Place::new_default_vec(4);
             let transitions = Transition::new_default_vec(5);
             let pre_arcs = vec![
-                Arc::new_cost_1(&places[0], &transitions[0]),
-                Arc::new_cost_1(&places[1], &transitions[1]),
-                Arc::new_cost_1(&places[2], &transitions[2]),
-                Arc::new_cost_1(&places[2], &transitions[3]),
-                Arc::new_cost_1(&places[3], &transitions[4]), 
+                places[0].link_cost_1(&transitions[0]),
+                places[1].link_cost_1(&transitions[1]),
+                places[2].link_cost_1(&transitions[2]),
+                places[2].link_cost_1(&transitions[3]),
+                places[3].link_cost_1(&transitions[4]),
             ];
             let post_arcs = vec![
-                Arc::new_cost_1(&places[1], &transitions[0]),
-                Arc::new_cost_1(&places[2], &transitions[1]),
-                Arc::new_cost_1(&places[0], &transitions[2]),
-                Arc::new_cost_1(&places[3], &transitions[3]),
-                Arc::new_cost_1(&places[2], &transitions[4]),
+                transitions[0].link_gain_1(&places[1]),
+                transitions[1].link_gain_1(&places[2]),
+                transitions[2].link_gain_1(&places[0]),
+                transitions[3].link_gain_1(&places[3]),
+                transitions[4].link_gain_1(&places[2]),                
             ];
             let petri2 = Petrinet::new(
                 "Petri2".to_string(),

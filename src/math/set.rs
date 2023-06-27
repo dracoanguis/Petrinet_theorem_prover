0bfadf184c6339ec3bcf9f801da73e53680d196e
Ethan Arm <ethanbargaudarm@gmail.com>
2023-06-27 20:03:12 +0200

    Added the doc of the set file

--- src/math/set.rs
+++ src/math/set.rs
@@ -1,13 +1,18 @@
+//! The set module, it defines the set object
+
 use std::{collections::HashSet};
 
 use super::{NNumber, Vector};
 
+
+/// A set of vectors over the NNumbers
 #[derive(Debug)]
 pub struct Set {
     data: Vec<Vector<NNumber>>,
 }
 
 impl Set {
+    /// Creates a Set from a HashSet where every 0 is converted to the N number
     pub fn new_0_into_n_set(input_set: HashSet<Vector<isize>>) -> Self {
         let mut data = Vec::new();
         data.reserve(input_set.len());
@@ -20,12 +25,14 @@ impl Set {
         s
     }
 
+    /// Creates a Set from a vec of Vector over NNumbers
     pub fn new_from_vec(data: Vec<Vector<NNumber>>) -> Self {
         let s = Self { data };
         println!("{}", s);
         s
     }
 
+    /// Creates a Set from a HashSet
     pub fn new_from_set(input_set: HashSet<Vector<isize>>) -> Self {
         let mut data = Vec::new();
         data.reserve(input_set.len());
@@ -36,28 +43,34 @@ impl Set {
         Self { data }
     }
 
+    /// Creates a new empty set
     pub fn new() -> Self {
         Self { data: Vec::new() }
     }
 
+    /// Check if the set is empty
     pub fn is_empty(&self) -> bool {
         self.data.is_empty()
     }
 
+    /// Check if the other vector is included in the current Set
     pub fn include(&self, other: &Vector<NNumber>) -> bool {
         self.data.iter().any(|v| v.include(other))
     }
 
+    /// Check if the other Set is included in the curretn Set
     pub fn include_set(&self, other: &Self) -> bool {
         other.data.iter().all(|v| self.include(v))
     }
 
+    /// Insert a vector over NNumbers in the current Set
     pub fn insert(&mut self, in_vect: Vector<NNumber>) {
         if !self.include(&in_vect) {
             self.data.push(in_vect);
         }
     }
 
+    /// Creates a Set that is the intersection between the current set and the given vector over NNumbers
     fn intersect_vec(&self, vec: &Vector<NNumber>) -> Set {
         Self {
             data: self
@@ -70,6 +83,7 @@ impl Set {
         }
     }
 
+    /// Creates a Set that is the intersection between the other Set and the current Set
     pub fn intersect(&self, other: &Self) -> Self {
         let b_s = if self.data.len() >= other.data.len() {
             (self, other)
@@ -89,6 +103,7 @@ impl Set {
         // Set { data: r_vec }
     }
 
+    /// Creates a Set that is the union between the current Set and the othe Set
     pub fn union(&self, other: &Self) -> Self {
         let b_s = if self.data.len() >= other.data.len() {
             (self, other)
@@ -107,6 +122,7 @@ impl Set {
         Self { data: n_data }
     }
 
+    /// Returns the size in elements of the Set
     pub fn len(&self) -> usize {
         self.data.len()
     }
